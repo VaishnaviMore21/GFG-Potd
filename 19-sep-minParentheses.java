d85f42ad92f34624189eb80f42a45a18fd6b6a25
Vaishnavi More <143414333+VaishnaviMore21@users.noreply.github.com>
2025-09-19 10:38:01 +0530

    Update 19-sep-minParentheses.java

--- 19-sep-minParentheses.java
+++ 19-sep-minParentheses.java
@@ -1,32 +1,27 @@
+
+
 class Solution {
     public int minParentheses(String s) {
-        // code here
-        Stack<Character>st=new Stack<>();
-         
-         for(int i=0;i<s.length();i++)
-         
-         {
-                char ch = s.charAt(i);
-             if(!st.isEmpty())
-             {
-                 if(ch=='(')
-                 {
-                     st.push(ch);
-                 }
-                 else if(ch==')')
-                 {
-                     st.pop();
-                 }
-                 else 
-                 {
-                      st.push(ch);
-                 }
-             }
-             else
-             {
-                  st.push(ch);
-             }
-         }
-         return st.size();
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!st.isEmpty()) {
+                if (ch == '(') {
+                    st.push(ch);
+                } else if (ch == ')') {
+                    // Only pop if top is matching '('
+                    if (st.peek() == '(') {
+                        st.pop();
+                    } else {
+                        st.push(ch);
+                    }
+                } else {
+                    st.push(ch);
+                }
+            } else {
+                st.push(ch);
+            }
+        }
+        return st.size();
     }
 }
